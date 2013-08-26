632020873a01df6df537f8e5c002054d74954a54
Razvan Bangu <razvan.bangu@gmail.com>
2013-08-26 14:41:19 -0400

    First push

---
+++ Razmatazz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Razmatazz
+//
+//  Created by Razvan Bangu on 2013-08-26.
+//  Copyright (c) 2013 Razvan Bangu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
